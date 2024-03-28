cbeafd6f9deab6cc0f3b1f53a307d97b56a7d01c
Vadim Tokar <tokar@dev.vtb.ru>
2024-03-28 09:03:27 +0300

    Add isLogged handling

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,13 +1,19 @@
+import { useAccount } from '@gear-js/react-hooks';
 import { NFTTokenList, NFTTokenItem } from 'entities/nft-token';
 import { CreateNFTToken } from 'features/create-nft-token';
 
 function Home() {
-  return (
+  const { account } = useAccount();
+  const isLogged = Boolean(account);
+
+  return isLogged ? (
     <NFTTokenList
       title="Your NFT tokens"
       actionSlot={<CreateNFTToken />}
       renderItem={(metadata, transactionId) => <NFTTokenItem metadata={metadata} key={transactionId} />}
     />
+  ) : (
+    <h2>Please, login to create your NFT tokens</h2>
   );
 }
 
